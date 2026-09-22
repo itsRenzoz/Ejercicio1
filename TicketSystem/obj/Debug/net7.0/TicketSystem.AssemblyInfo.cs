52c244d2537deb1547a8d908ab125f031d212e7d
JafethPM <jpoveda80174@ufide.ac.cr>
2026-09-21 22:13:33 -0600

    Cambio necesarios

--- TicketSystem/obj/Debug/net7.0/TicketSystem.AssemblyInfo.cs
+++ TicketSystem/obj/Debug/net7.0/TicketSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5cb75a07e27aa77856e39c5b501aab68a945aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
